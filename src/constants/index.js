2046b559c152e3cdfc3e93f43e61505d8b6a67d7
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2025-01-08 20:27:20 +0100

    deleted contact

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,15 +5,6 @@ export const SORT_ORDER = {
   DESC: 'desc',
 };
 
-export const CONTACT_TYPE = {
-  WORK: 'work',
-  HOME: 'home',
-  PERSONAL: 'personal',
-};
-
-export const DEFAULT_PERPAGE = 4;
-export const DEFAULT_FIRSTPAGE = 1;
-
 export const FIFTEEN_MINUTES = 150 * 60 * 1000;
 export const ONE_MONTH = 30 * 24 * 60 * 60 * 1000;
 
